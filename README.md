278a53997a1dad161ae71e938834b6a8ed12b343
Romain Bessuges-Meusy <romain.bessuges@main-gauche.com>
2020-05-30 15:03:37 +0200

    Add doc, rename GATBY_AUTH0_CLIENTID and merge PR.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # netlify-plugin-auth0-patch-urls
+
 A Netlify Build plugin that makes Auth0 works with deploy previews
+
+## Motivation 
+
+Auth0 doesn't allow wildcard subdomains so things get rapidly complicated when using their SPA library while deploying
+previews on Netlify. This build plugin automates the Auth0 Application configuration update by leveraging the Auth0 
+management API. *It basically adds the preview URLs of current Netlify build to the list of authorized URLs.* 
+
+## Prerequisites
+
+1. In order to get it to work properly, it is required to create a Management API token in 
+the APIs section of your Auth0 account, and grant it the `read:clients` and `update:clients` permission.
+
+2. Then you need to create a dedicated Application ("achine to machine") and choose the previously created API in the APIs
+tab.
+
+3. Lastly, you have to declare the environment variables that will contain all the client ID and secrets required by this
+build plugin:
+
+Env Variable | Description
+------------ | --------------
+`AUTH0_DOMAIN` | This is you Aut0 application domain 
+`AUTH0_CLIENT_ID` | Client ID for the SPA Application
+`AUTH0_CALLBACK_PATHNAMES` | Url pathnames that will be appended for callback
+`AUTH0_LOGOUT_PATHNAMES` |Url pathnames that will be appended for logouts
+`AUTH0_MANAGEMENT_CLIENT_ID` |  Client ID of the Management Application
+`AUTH0_MANAGEMENT_CLIENT_SECRET` | Client Secret of the Management Application
+`DEPLOY_URL` | Specified by Netlify build
+
+These variables have to be defined in your Netlify deployments settings
+
+## Installation 
+
+Install locally (it seems to help with Netlify build time).
+
+```
+$ yarn add netlify-plugin-auth0-patch-urls
+```
+
+Add the plugin to the `netlify.toml`
+
+```toml
+[[plugins]]
+  package = "netlify-plugin-auth0-patch-urls"
+```
+
--- index.js
+++ index.js
@@ -1,107 +1,113 @@
 /* eslint-disable no-console */
-const path = require('path');
-const { ManagementClient } = require('auth0');
-require('dotenv').config({
-    path: path.resolve(process.cwd(), `.env.${process.env.CONTEXT}`),
+const path = require("path");
+const { ManagementClient } = require("auth0");
+require("dotenv").config({
+  path: path.resolve(process.cwd(), `.env.${process.env.CONTEXT}`),
 });
 
 module.exports = {
-    onPreBuild: ({ utils }) => {
-        console.info(`🔑 Auth0 Plugin startup 🔑`);
-        const tab = '   ';
-        const requiredEnvVariables = [
-            'AUTH0_DOMAIN',
-            'AUTH0_MANAGEMENT_CLIENT_ID',
-            'AUTH0_MANAGEMENT_CLIENT_SECRET',
-            'GATSBY_AUTH0_CLIENTID',
-            'AUTH0_CALLBACK_PATHNAMES',
-            'AUTH0_LOGOUT_PATHNAMES',
-            'DEPLOY_URL',
-        ];
-        const missingEnvVariables = requiredEnvVariables.filter(
-            envVar => typeof process.env[envVar] === 'undefined',
+  onPreBuild: ({ utils }) => {
+    console.log(`🔑 Auth0 Plugin startup`);
+    const tab = "   ";
+    const requiredEnvVariables = [
+      "AUTH0_DOMAIN",
+      "AUTH0_MANAGEMENT_CLIENT_ID",
+      "AUTH0_MANAGEMENT_CLIENT_SECRET",
+      "AUTH0_CLIENT_ID",
+      "AUTH0_CALLBACK_PATHNAMES",
+      "AUTH0_LOGOUT_PATHNAMES",
+      "DEPLOY_URL",
+    ];
+    const missingEnvVariables = requiredEnvVariables.filter(
+      (envVar) => typeof process.env[envVar] === "undefined"
+    );
+
+    if (missingEnvVariables.length > 0) {
+      utils.build.failPlugin(
+        `${tab} ☠️ Missing environment variables: ${missingEnvVariables.join(
+          ", "
+        )}`
+      );
+    }
+
+    return new Promise((resolve) => {
+      const url = process.env.DEPLOY_URL;
+      console.log(`${tab} 🧭 Deploy Preview URL should be:`, url);
+      const deployPrimeUrl = process.env.DEPLOY_PRIME_URL;
+      if (deployPrimeUrl) {
+        console.log(
+          `${tab} 🧭 Deploy Preview Prime URL should be:`,
+          deployPrimeUrl
         );
+      }
 
-        if (missingEnvVariables.length > 0) {
-            utils.build.failPlugin(
-                `${tab} ☠️ Missing environment variables: ${missingEnvVariables.join(
-                    ', ',
-                )}`,
+      const management = new ManagementClient({
+        domain: process.env.AUTH0_DOMAIN,
+        clientId: process.env.AUTH0_MANAGEMENT_CLIENT_ID,
+        clientSecret: process.env.AUTH0_MANAGEMENT_CLIENT_SECRET,
+        scope: "read:clients update:clients",
+      });
+
+      management.clients
+        .get({ client_id: process.env.AUTH0_CLIENT_ID })
+        .then((client) => {
+          console.log(`${tab} 🗝 Retrieved Auth0 client:`, client.name);
+          if (client.allowed_clients.indexOf(url) === -1) {
+            console.log(`${tab} URL has not yet been added to Auth0 Client`);
+            const getComposeUrls = (envKey, urlToUse) =>
+              urlToUse
+                ? process.env[envKey]
+                    .split(",")
+                    .map((pathname) => `${urlToUse}/${pathname}`)
+                : [];
+            const urlOrigins = deployPrimeUrl ? [url, deployPrimeUrl] : [url];
+            const getArrayWithUniqueValues = (arr) => [...new Set(arr)];
+            const payload = {
+              allowed_clients: getArrayWithUniqueValues(
+                client.allowed_clients.concat(urlOrigins)
+              ),
+              web_origins: getArrayWithUniqueValues(
+                client.web_origins.concat(urlOrigins)
+              ),
+              allowed_origins: getArrayWithUniqueValues(
+                client.allowed_origins.concat(urlOrigins)
+              ),
+              callbacks: getArrayWithUniqueValues(
+                client.callbacks.concat([
+                  ...getComposeUrls("AUTH0_CALLBACK_PATHNAMES", url),
+                  ...getComposeUrls("AUTH0_CALLBACK_PATHNAMES", deployPrimeUrl),
+                ])
+              ),
+              allowed_logout_urls: getArrayWithUniqueValues(
+                client.callbacks.concat([
+                  ...getComposeUrls("AUTH0_LOGOUT_PATHNAMES", url),
+                  ...getComposeUrls("AUTH0_LOGOUT_PATHNAMES", deployPrimeUrl),
+                ])
+              ),
+            };
+            management.clients.update(
+              { client_id: process.env.AUTH0_CLIENT_ID },
+              payload,
+              (updateError, updatedClient) => {
+                if (updateError) {
+                  utils.build.failPlugin(
+                    `${tab} ☠️ Something wrong happened while trying to patch Auth0 Client`
+                  );
+                } else {
+                  console.log(`${tab} 🍾 Successfully patched Auth0 Client.`);
+                  console.log(
+                    `${tab} Allowed URLS:`,
+                    updatedClient.allowed_clients
+                  );
+                }
+                resolve();
+              }
             );
-        }
-
-        return new Promise(resolve => {
-            const url = process.env.DEPLOY_URL;
-            console.log(`${tab} 🧭 Deploy Preview URL should be:`, url);
-            const deployPrimeUrl = process.env.DEPLOY_PRIME_URL;
-            if (deployPrimeUrl) {
-                console.log(`${tab} 🧭 Deploy Preview Prime URL should be:`, deployPrimeUrl);
-            }
-
-            const management = new ManagementClient({
-                domain: process.env.AUTH0_DOMAIN,
-                clientId: process.env.AUTH0_MANAGEMENT_CLIENT_ID,
-                clientSecret: process.env.AUTH0_MANAGEMENT_CLIENT_SECRET,
-                scope: 'read:clients update:clients',
-            });
-
-            management.clients
-                .get({ client_id: process.env.GATSBY_AUTH0_CLIENTID })
-                .then(client => {
-                    console.info(`${tab} 🗝 Retrieved Auth0 client:`, client.name);
-                    if (client.allowed_clients.indexOf(url) === -1) {
-                        console.info(`${tab} URL has not yet been added to Auth0 Client`);
-                        const getComposeUrls = (envKey, urlToUse) => urlToUse
-                            ? process.env[envKey].split(',').map(
-                                pathname => `${urlToUse}/${pathname}`,
-                            )
-                            : [];
-                        const urlOrigins = deployPrimeUrl
-                            ? [url, deployPrimeUrl]
-                            : [url];
-                        const getArrayWithUniqueValues = arr => [...new Set(arr)];
-                        const payload = {
-                            allowed_clients: getArrayWithUniqueValues(client.allowed_clients.concat(urlOrigins)),
-                            web_origins: getArrayWithUniqueValues(client.web_origins.concat(urlOrigins)),
-                            allowed_origins: getArrayWithUniqueValues(client.allowed_origins.concat(urlOrigins)),
-                            callbacks: getArrayWithUniqueValues(client.callbacks.concat(
-                                [
-                                    ...getComposeUrls('AUTH0_CALLBACK_PATHNAMES', url)
-                                    ...getComposeUrls('AUTH0_CALLBACK_PATHNAMES', deployPrimeUrl)
-                                ]
-                            )),
-                            allowed_logout_urls: getArrayWithUniqueValues(client.callbacks.concat(
-                                [
-                                    ...getComposeUrls('AUTH0_LOGOUT_PATHNAMES', url)
-                                    ...getComposeUrls('AUTH0_LOGOUT_PATHNAMES', deployPrimeUrl)
-                                ]
-                            )),
-                        };
-                        management.clients.update(
-                            { client_id: process.env.GATSBY_AUTH0_CLIENTID },
-                            payload,
-                            (updateError, updatedClient) => {
-                                if (updateError) {
-                                    utils.build.failPlugin(
-                                        `${tab} ☠️ Something wrong happened while trying to patch Auth0 Client`,
-                                    );
-                                } else {
-                                    console.log(`${tab} 🍾 Successfully patched Auth0 Client.`);
-                                    console.log(
-                                        `${tab} Allowed URLS:`,
-                                        updatedClient.allowed_clients,
-                                    );
-                                }
-                                resolve();
-                            },
-                        );
-                    } else {
-                        console.info(
-                            `${tab} 👍 URL has already been added to Auth0 Client`,
-                        );
-                        resolve();
-                    }
-                });
+          } else {
+            console.log(`${tab} 👍 URL has already been added to Auth0 Client`);
+            resolve();
+          }
         });
-    },
+    });
+  },
 };
